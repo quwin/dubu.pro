3acee4b082745a3609b2bd3f6a26525bfc43dba8
Ethan Tran <ethantran.et77@gmail.com>
2023-07-20 15:39:20 +0000

    Barrier Toggles implemented (except Demon Dais)

--- jquery.js
+++ jquery.js
@@ -3,65 +3,155 @@ $(function() {
 });
 
 const canvas = document.getElementById('drawing-board');
+const background = document.getElementById('background');
 const toolbar = document.getElementById('toolbar');
 const ctx = canvas.getContext('2d');
+const backgroundCtx = background.getContext('2d');
 
 const canvasOffsetX = canvas.offsetLeft;
 const canvasOffsetY = canvas.offsetTop;
 const characters = ['aimi', 'asher', 'atlas', 'drekar', 'dubu', 'era', 'estelle', 'finii', 'juliette', 'juno', 'kai', 'luna', 'octavia', 'rasmus', 'rune', 'vyce', 'x', 'zentaro'];
+const maps = ['images/Ai.Mis App.png', 'images/Ahten City.png', 'images/Demon Dais.png', 'images/Atlas Lab.png', 'images/Oni Village.png', 'images/Night Market.png'];
 
 
 canvas.width = window.innerWidth - canvasOffsetX;
 canvas.height = window.innerHeight - canvasOffsetY;
+background.width = window.innerWidth - canvasOffsetX;
+background.height = window.innerHeight - canvasOffsetY;
 
 let isPainting = false;
 let lineWidth = 3;
 let startX;
 let startY;
-let map = document.getElementById("onipng");
-let barriers = true;
+let mapindex = 5;
+let barrierslefttop = true;
+let barriersrighttop = true;
+let barriersleftbot = true;
+let barriersrightbot = true;
 let ally = true;
+
+var img = new Image();
+img.onload = function() {
+    backgroundCtx.drawImage(img, 768, 0, 768*3, 2160, background.width/5, 0, background.width*3/5, background.width*9/16);
+    barrierCheck();
+};
+img.src = maps[mapindex];
 
 toolbar.addEventListener('click', e => {
     if (e.target.id === 'clear') {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-    if (e.target.id === 'barriers') {
-      barriers = !barriers;
+    if (e.target.id === 'barrierslefttop') {
+      barrierslefttop = !barrierslefttop;
+      barrierCheck();
+    }
+    if (e.target.id === 'barriersrighttop') {
+      barriersrighttop = !barriersrighttop;
+      barrierCheck();
+    }
+    if (e.target.id === 'barriersleftbot') {
+      barriersleftbot = !barriersleftbot;
+      barrierCheck();
+    }
+    if (e.target.id === 'barriersrightbot') {
+      barriersrightbot = !barriersrightbot;
+      barrierCheck();
     }
     if (e.target.id === 'ally') {
       ally = !ally;
     }
 
-    // if (e.target.id === 'oni') {
-    //   map = document.getElementById("onipng");
-    //   ctx.drawImage(map,0,0,canvas.width, canvas.width*9/16);
-    // }
-    // if (e.target.id === 'ahten') {
-    //   map = document.getElementById("ahtenpng");
-    //   ctx.drawImage(map,0,0,canvas.width, canvas.width*9/16);
-    //   if (!barriers) {
-    //     ctx.drawImage(document.getElementById("ahtenleft"),0,0,canvas.width/5, canvas.width*9/16);
-    //     ctx.drawImage(document.getElementById("ahtenright"),canvas.width*4/5,0,canvas.width/5, canvas.width*9/16);
-    //   }
-    // }
-    // if (e.target.id === 'aimiapp') {
-    //   map = document.getElementById("aimipng");
-    //   ctx.drawImage(map,0,0,canvas.width, canvas.width*9/16);
-    // }
-    // if (e.target.id === 'atlaslab') {
-    //   map = document.getElementById("atlaspng");
-    //   ctx.drawImage(map,0,0,canvas.width, canvas.width*9/16);
-    // }
-    // if (e.target.id === 'night') {
-    //   map = document.getElementById("nightpng");
-    //   ctx.drawImage(map,0,0,canvas.width, canvas.width*9/16);
-    // }
-    // if (e.target.id === 'demon') {
-    //   map = document.getElementById("demonpng");
-    //   ctx.drawImage(map,0,0,canvas.width, canvas.width*9/16);
-    // }
+    if (e.target.id === 'oni') {
+      mapindex = 4;
+      img.src = maps[mapindex];
+    }
+    if (e.target.id === 'ahten') {
+      mapindex = 1;
+      img.src = maps[mapindex];
+    }
+    if (e.target.id === 'aimiapp') {
+      mapindex = 0;
+      img.src = maps[mapindex];
+    }
+    if (e.target.id === 'atlaslab') {
+      mapindex = 3;
+      img.src = maps[mapindex];
+    }
+    if (e.target.id === 'night') {
+      mapindex = 5;
+      img.src = maps[mapindex];
+    }
+    if (e.target.id === 'demon') {
+      mapindex = 2;
+      img.src = maps[mapindex];
+    }
 });
+
+function barrierCheck(){
+  if (mapindex == 2) {
+    clipImgSide(0, 0, 1280, 2160, 0, 0, background.width/3, background.width*9/16);
+    clipImgSide(1280*2, 0, 1280, 2160, background.width*2/3, 0, background.width/3, background.width*9/16)
+  }
+  else if (mapindex < 2) {
+    if (!barrierslefttop) {
+      imgSide(0, 0, background.width/5, background.width*9/16, ' left.png');
+    }
+    else {
+      clipImgSide(0, 0, 768, 2160, 0, 0, background.width/5, background.width*9/16)
+    }
+    if (!barriersrighttop) {
+      imgSide(background.width*4/5, 0, background.width/5, background.width*9/16, ' right.png');
+    }
+    else {
+      clipImgSide(768*4, 0, 768, 2160, background.width*4/5, 0, background.width/5, background.width*9/16)
+    }
+  }
+  else {
+    if (!barrierslefttop && !!!barriersleftbot) {
+      imgSide(0, 0, background.width/3, background.width*9/16, ' left.png');
+    }
+    else if (!barrierslefttop) {
+      imgSide(0, 0, background.width/3, background.width*9/16, ' left top.png');
+    }
+    else if (!barriersleftbot) {
+      imgSide(0, 0, background.width/3, background.width*9/16, ' left bot.png');
+    }
+    else {
+      clipImgSide(0, 0, 1280, 2160, 0, 0, background.width/3, background.width*9/16)
+    }
+    if (!barriersrighttop && !barriersrightbot) {
+      imgSide(background.width*2/3, 0, background.width/3, background.width*9/16, ' right.png');
+    }
+    else if (!barriersrighttop) {
+      imgSide(background.width*2/3, 0, background.width/3, background.width*9/16, ' right top.png');
+    }
+    else if (!barriersrightbot) {
+      imgSide(background.width*2/3, 0, background.width/3, background.width*9/16, ' right bot.png');
+    }
+    else {
+      clipImgSide(1280*2, 0, 1280, 2160, background.width*2/3, 0, background.width/3, background.width*9/16)
+    }
+  }
+}
+
+function imgSide(x, y, width, height, string) {
+  let imgside = new Image();
+
+  imgside.onload = function () {
+    backgroundCtx.drawImage(imgside, x, y, width, height);
+  }
+  imgside.src = img.src.slice(0, img.src.length - 4) + string;
+}
+
+function clipImgSide(sx, sy, swidth, sheight, x, y,  width, height) {
+  let imgside = new Image();
+
+  imgside.onload = function () {
+    backgroundCtx.drawImage(imgside, sx, sy, swidth, sheight, x, y,  width, height);
+  }
+  imgside.src = img.src;
+}
+
 
 toolbar.addEventListener('mousedown', e => {
   if (characters.includes(e.target.id)) {
